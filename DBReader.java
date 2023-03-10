cc3c754a96b52d8dbba6a45404e0b74c8df6993c
Fukang YANG <2727182y@student.gla.ac.uk>
2023-03-10 23:43:21 +0000

    基础的DBreader

--- DBReader.java
+++ DBReader.java
@@ -1,3 +1,20 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import java.io.File;
+
 public class DBReader{
-    
+    public static void main(String argv[]) {
+   
+        try {
+           File inputFile = new File("Database.xml");
+           DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+           Document doc = dBuilder.parse(inputFile);
+           doc.getDocumentElement().normalize();
+           System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+     }    
 }
